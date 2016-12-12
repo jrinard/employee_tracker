0724dab0a57ad8af312958b078ef2d9e3e48b67a
Joshua Rinard and Marty Kovach <Guest@epicodus-87.local>
2016-12-12 14:28:27 -0800

    first passing spec

--- spec/division_spec.rb
+++ spec/division_spec.rb
@@ -2,11 +2,11 @@ require("spec_helper")
 
 describe(Division) do
   describe("#employees") do
-    it("tells which tasks are in it") do
-      division = Division.create({:name => "HR"})
-      employee1 = Employee.create({:name => "Barry Allan", :division_id => division.id})
-      employee2 = Employee.create({:name => "Bruce Wayne", :division_id => division.id})
-    expect(division.employees()).to(eq([employee1, employee2]))
+    it("tells which employees are in it") do
+      test_division = Division.create({:name => "HR"})
+      employee1 = Employee.create({:name => "Barry Allan", :division_id => test_division.id})
+      employee2 = Employee.create({:name => "Bruce Wayne", :division_id => test_division.id})
+    expect(test_division.employees()).to(eq([employee1, employee2]))
     end
   end
 end
