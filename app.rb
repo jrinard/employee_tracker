8d446279b00065a1c20299fb0bdff0ce90eacee5
Joshua Rinard and Marty Kovach <Guest@epicodus-87.local>
2016-12-12 14:42:06 -0800

    starting routing

--- app.rb
+++ app.rb
@@ -9,3 +9,18 @@ require("pg")
 get("/") do
   erb(:index)
 end
+
+get("/divisions/new") do
+  erb(:division_form)
+end
+
+get('/divisions') do
+  @divisions = Division.all()
+  erb(:divisions)
+end
+
+post("/divisions") do
+  name = params.fetch("name")
+  division = Division.create({:name => name, :id => nil})
+   redirect "/divisions"
+end
